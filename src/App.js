5b0befac0394f82b1e4e5ac75bb03ef513d1ede0
tanmay-choudhary <tanmaychoudhary4102@gmail.com>
2021-11-10 15:18:50 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { withRouter } from 'react-router-dom';
+import Routes from "./Routes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    const childProps = {  
+    };
+
+    return (
+      <div> 
+        <Routes childProps={childProps} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default withRouter(App);
+//tanmay
